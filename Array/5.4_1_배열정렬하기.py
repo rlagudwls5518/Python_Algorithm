561b31c097357c295f25a8fc0242cde03464bcae
rlagudwls5518 <rlagudwls5518@naver.com>
2025-09-15 15:15:36 +0900

    09/15(1)

---
+++ Array/5.4_1_배열정렬하기.py
@@ -0,0 +1,11 @@
+#정수배열을 오름차순으로 정렬해서 반환하는 solution함수를 완성하라
+import  sys
+
+input =  sys.stdin.readline()
+
+
+
+def solution(arr):
+    return arr.sort()
+
+
---
+++ Array/5.4_2_배열제어하기.py
@@ -0,0 +1,8 @@
+#정수 배열을 하나받고 배열의 중복값을 제거하고 배열 데이터를 
+#내림차순으로 정렬해서 반환하는 함수를 구현
+
+
+def solution(arr):
+    set(arr)
+    arr.sort(reverse=True)
+    return arr
